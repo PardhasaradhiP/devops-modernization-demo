de42383f4d0ff64f189cea7d45a16cd1a3a802f7
PardhasaradhiP <paruchuriPardhasaradhi@gmail.com>
2025-11-23 19:09:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 def calculate_bill(amount):
     # Simulates migrated business logic
-    return amount * 1.18  # 18% tax
+    return amount * 1.20  # 18% tax
 
 if __name__ == "__main__":
     print("Bill amount:", calculate_bill(100))
